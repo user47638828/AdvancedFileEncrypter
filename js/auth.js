14570bc5dea32c990a93f19f7a23c0ef22e91d91
Know me? <he72nkhe5biw7@gmail.com>
2026-03-12 17:14:56 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,10 @@
-document.getElementById("loginBtn").addEventListener("click", function() {
+const loginBtn = document.getElementById("loginBtn");
+loginBtn.addEventListener("click", ()=>{
     const pwd = document.getElementById("password").value;
-    if(pwd === "supersecret") {  // Passwort für Version 1
-        window.location.href = "encrypt.html";
+    if(pwd === "supersecret"){
+        document.getElementById("loginPanel").style.display="none";
+        document.getElementById("toolPanel").style.display="block";
+        document.getElementById("encryptTab").classList.add("active");
     } else {
         alert("Falsches Passwort!");
     }
