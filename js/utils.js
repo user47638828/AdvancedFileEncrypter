a6dd35200fe093d633350f6a41f06bdc6fa32d39
Know me? <he72nkhe5biw7@gmail.com>
2026-03-12 16:56:01 +0100

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,15 @@
+function arrayBufferToBase64(buffer){
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for(let i=0;i<len;i++) binary += String.fromCharCode(bytes[i]);
+    return btoa(binary);
+}
+
+function base64ToArrayBuffer(base64){
+    const binary = atob(base64);
+    const len = binary.length;
+    const bytes = new Uint8Array(len);
+    for(let i=0;i<len;i++) bytes[i] = binary.charCodeAt(i);
+    return bytes.buffer;
+}
